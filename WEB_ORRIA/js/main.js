dc485b020c87161818367a84c3b1eb71b2a534c9
Ikmdiaz25 <ikmdiaz25@lhusurbil.eus>
2026-02-10 10:42:09 +0100

    bigarren modeloa

--- WEB_ORRIA/js/main.js
+++ WEB_ORRIA/js/main.js
@@ -1,43 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const track = document.querySelector('.carousel-track');
-    const slides = Array.from(track.children);
-    const nextButton = document.querySelector('.next-btn');
-    const prevButton = document.querySelector('.prev-btn');
+    const menuBotoia = document.getElementById('menu-ireki');
+    const menuLeihoa = document.getElementById('menu-leihoa');
+    const itxiBotoia = document.getElementById('menu-itxi');
 
-    // Position slides
-    const moveToSlide = (track, currentSlide, targetSlide) => {
-        const targetIndex = slides.findIndex(slide => slide === targetSlide);
-        track.style.transform = `translateX(-${targetIndex * 100}%)`;
-        currentSlide.classList.remove('current-slide');
-        targetSlide.classList.add('current-slide');
+    if (menuBotoia && menuLeihoa) {
+        menuBotoia.addEventListener('click', () => {
+            menuLeihoa.showModal();
+        });
     }
 
-    // Set first slide as current
-    slides[0].classList.add('current-slide');
+    if (itxiBotoia && menuLeihoa) {
+        itxiBotoia.addEventListener('click', () => {
+            menuLeihoa.close();
+        });
+    }
 
-    // Button listeners
-    nextButton.addEventListener('click', e => {
-        const currentSlide = track.querySelector('.current-slide');
-        let nextSlide = currentSlide.nextElementSibling;
-        if (!nextSlide) nextSlide = slides[0];
+    if (menuLeihoa) {
+        menuLeihoa.addEventListener('click', (e) => {
+            const rect = menuLeihoa.getBoundingClientRect();
+            const isInDialog = (rect.top <= e.clientY && e.clientY <= rect.top + rect.height &&
+                rect.left <= e.clientX && e.clientX <= rect.left + rect.width);
 
-        moveToSlide(track, currentSlide, nextSlide);
-    });
-
-    prevButton.addEventListener('click', e => {
-        const currentSlide = track.querySelector('.current-slide');
-        let prevSlide = currentSlide.previousElementSibling;
-        if (!prevSlide) prevSlide = slides[slides.length - 1];
-
-        moveToSlide(track, currentSlide, prevSlide);
-    });
-
-    // Auto play
-    setInterval(() => {
-        const currentSlide = track.querySelector('.current-slide');
-        let nextSlide = currentSlide.nextElementSibling;
-        if (!nextSlide) nextSlide = slides[0];
-
-        moveToSlide(track, currentSlide, nextSlide);
-    }, 5000);
+        });
+    }
 });
