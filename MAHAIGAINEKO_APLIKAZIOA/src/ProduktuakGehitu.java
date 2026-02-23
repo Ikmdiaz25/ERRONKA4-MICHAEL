ba0528f5c592db2b3103388286e7387e9f103ea4
Ikmdiaz25 <ikmdiaz25@lhusurbil.eus>
2026-02-23 09:09:21 +0100

    1.commit

--- MAHAIGAINEKO_APLIKAZIOA/src/ProduktuakGehitu.java
+++ MAHAIGAINEKO_APLIKAZIOA/src/ProduktuakGehitu.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class ProduktuakGehitu {
 
+    public void produktuaGehitu(Scanner sc) {
+
+        System.out.println("Sartu produktuaren izena");
+        String prodIzena = sc.nextLine();
+
+        System.out.println("Sartu produktuaren deskribapena");
+        String prodDeskribapena = sc.nextLine();
+
+        System.out.println("Sartu produktuaren prezioa");
+        double prodPrezioa = sc.nextDouble();
+
+        System.out.println("Sartu produktuaren stocka");
+        int prodStock = sc.nextInt();
+
+        System.out.println("Sartu produktuaren kategoria");
+        String prodKategoria = sc.nextLine();
+
+        System.out.println("Sartu produktuaren irudia");
+        String prodIrudia = sc.nextLine();
+
+    }
 }
